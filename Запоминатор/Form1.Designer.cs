4f0b2d9fed3634170d4d69e6930daca8ce0c676e
17point53 <Konstantin-as@mail.ru>
2022-07-12 15:43:18 +0500

    Добавьте файлы проекта.

---
+++ Запоминатор/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Учитель_английского
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(437, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Сменить язык";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(504, 85);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Продолжить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(575, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Счетчик";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(363, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 37);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Показать слово";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(437, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Слово";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(659, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 147);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Rememberer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+    }
+}
+
---
+++ Запоминатор/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Учитель_английского
+{
+    public partial class Form1 : Form
+    {
+        Random Rnd = new Random();
+        Dictionary<string, string> GeneralDict = new Dictionary<string, string>();
+        Dictionary<string, string> CurrentDict = new Dictionary<string, string>();
+        int number;
+        string answer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox2.BackColor = Color.White;
+            label1.Visible = false;
+            label2.Visible = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            if (GeneralDict.Count != 0)
+            {
+                button5.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Открыть файл
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            label2.Visible = false;
+            ofd.Filter = "txt files (*.txt)|*.txt";
+            ofd.ShowDialog();
+            string path = ofd.FileName;
+            
+            if (path != "")
+            {
+                CurrentDict.Clear();
+                GeneralDict.Clear();
+                FillOutDictionaries(path);
+                Start();
+            }
+            ofd = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        } // Выход
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            button5.Enabled = true;
+        
+            ChangeLanguage();
+            
+            number = Rnd.Next(0, CurrentDict.Count - 1);
+            textBox1.Text = CurrentDict.ElementAt(number).Value;
+            answer = CurrentDict.ElementAt(number).Key;
+
+            textBox2.Clear();
+            textBox2.BackColor = Color.White;
+            label1.Text = CurrentDict.Count + "/" + GeneralDict.Count;
+            label1.Visible = true;
+            textBox2.Focus();
+        } //Сменить язык
+
+        private void button4_Click(object sender, EventArgs e) // Продолжить
+        {
+            if (textBox2.Text.ToLower() == answer)
+            {
+                label2.Visible = false;
+                button5.Enabled = true;
+                
+                textBox2.Clear();
+                textBox2.BackColor = Color.White;
+                label1.Text = CurrentDict.Count - 1 + "/" + GeneralDict.Count;
+                CurrentDict.Remove(answer);
+
+                ChangeWord();
+            }
+            else
+            {
+                textBox2.BackColor = Color.White;
+                Thread.Sleep(250);
+                textBox2.BackColor = Color.Red;
+                textBox2.Clear();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Показать слово
+        {
+            button5.Enabled = false;
+            label2.Text = answer;
+            label2.Visible = true;
+        }
+
+        private void FillOutDictionaries (string path)
+        {
+            StreamReader ReadFile = new StreamReader(path);
+            string Word;
+            string[] Words;
+
+            try
+            {
+                do
+                {
+                Word = ReadFile.ReadLine();
+                
+                    if (Word != null)
+                    {
+                        Word = Word.ToLower();
+                        Words = Word.Split('-').ToArray();
+                        Words[0] = Words[0].TrimEnd(' ');
+                        Words[1] = Words[1].TrimEnd(' ');
+                        Words[0] = Words[0].TrimStart(' ');
+                        Words[1] = Words[1].TrimStart(' ');
+
+                        if (Words.Length == 2)
+                        {
+                            GeneralDict.Add(Words[0], Words[1]);
+                            CurrentDict.Add(Words[0], Words[1]);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Выбран неверный файл");
+                            return;
+                        }
+                    }
+                }
+                while (Word != null);
+
+            }
+                catch
+            {
+                MessageBox.Show("Выбран неверный файл");
+            }
+        }
+
+        private void Start ()
+        {
+            if (GeneralDict.Count != 0)
+            {
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+
+                number = Rnd.Next(0, CurrentDict.Count - 1);
+                textBox1.Text = CurrentDict.ElementAt(number).Value;
+                answer = CurrentDict.ElementAt(number).Key;
+
+                label1.Visible = true;
+                label1.Text = CurrentDict.Count + "/" + GeneralDict.Count;
+                textBox2.Enabled = true;
+                textBox2.Focus();
+            }
+        }
+
+        private void ChangeLanguage ()
+        {
+            CurrentDict.Clear();
+
+            for (int i = 0; i < GeneralDict.Count; i++)
+            {
+                try
+                {
+                    CurrentDict.Add(GeneralDict.ElementAt(i).Value, GeneralDict.ElementAt(i).Key);
+                }
+                catch
+                {
+                    CurrentDict.Add(GeneralDict.ElementAt(i).Value + 1, GeneralDict.ElementAt(i).Key);
+                }
+            }
+            GeneralDict.Clear();
+
+            for (int i = 0; i < CurrentDict.Count; i++)
+            {
+                GeneralDict.Add(CurrentDict.ElementAt(i).Key, CurrentDict.ElementAt(i).Value);
+            }
+        }
+
+        private void ChangeWord ()
+        {
+            if (CurrentDict.Count > 0)
+            {
+                number = Rnd.Next(0, CurrentDict.Count - 1);
+                textBox1.Text = CurrentDict.ElementAt(number).Value;
+                answer = CurrentDict.ElementAt(number).Key.ToLower();
+            }
+            else
+            {
+                foreach (var a in GeneralDict) CurrentDict.Add(a.Key, a.Value);
+
+                number = Rnd.Next(0, CurrentDict.Count - 1);
+                while (CurrentDict.ElementAt(number).Key == answer)
+                {
+                    number = Rnd.Next(0, CurrentDict.Count - 1);
+                }
+
+                label1.Text = CurrentDict.Count + "/" + GeneralDict.Count;
+                textBox1.Text = CurrentDict.ElementAt(number).Value;
+                answer = CurrentDict.ElementAt(number).Key.ToLower();
+            }
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Формат входного файла: \r'word - перевод'");
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button4_Click(this, EventArgs.Empty);
+            }
+        }
+    }
+}
